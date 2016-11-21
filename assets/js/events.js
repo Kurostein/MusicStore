ff31c5749a9cad1c0b409f540db039b81f8325bf
Rastelli <felipe-rastelli@hotmail.com>
2016-11-20 21:36:16 -0300

    Album details page added. Some minor details adjusted.

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,3 +1,9 @@
-(function(){
-  
+(function () {
+  var container = this;
+
+  container.onItemClick = OnItemClick;
+
+  function OnItemClick() {
+    document.location.href = './pages/album_detail.html';
+  }
 })();
